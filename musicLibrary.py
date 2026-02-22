5551d78aad03c74a2759678d6b2ab7783b053572
Sandy0139 <sandeepsj8088@gmail.com>
2026-02-22 20:05:47 +0530

    Initial clean commit

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music={
+    "banjara":"http://youtube.com/watch?v=0NFxcNheoLc&list=RD0NFxcNheoLc&start_radio=1",
+    "galiyan":"https://www.youtube.com/watch?v=FxAG_11PzCk&list=RDFxAG_11PzCk&start_radio=1",
+    "striver":"https://www.youtube.com/watch?v=37E9ckMDdTk&list=PLgUwDviBIf0rENwdL0nEH0uGom9no0nyB"
+}
