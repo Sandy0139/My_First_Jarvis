0836bdc7cda20e0a7075a9c8e42858f748e2a6a7
Sandy0139 <sandeepsj8088@gmail.com>
2026-02-22 20:10:06 +0530

    Clean initial commit

---
+++ client.py
@@ -0,0 +1,16 @@
+# this is dumb code
+
+from openai import OpenAI
+
+client=OpenAI(
+api_key = os.getenv("OPENAI_API_KEY"))
+
+completion=client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages = [
+    {"role": "system", "content": "You are a virtual assistant, skilled in explaining general tasks like alexa and google cloud."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+]
+)
+
+print(completion.choices[0].message.content)
